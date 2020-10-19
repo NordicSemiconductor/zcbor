22eeb919521fbed3cc0429009af730b9884373a8
Fabio Utzig <fabio.utzig@nordicsemi.no>
2020-10-19 19:46:31 -0300

    tests: fix error checks in tests scripts
    Add proper error checking to each command invoked by the test scripts
    under tests/cbor_[encode|decode].
    
    Signed-off-by: Fabio Utzig <fabio.utzig@nordicsemi.no>

--- tests/cbor_decode/test.sh
+++ tests/cbor_decode/test.sh
@@ -3,11 +3,24 @@
 for dir in 'test1_suit_old_formats/' 'test2_suit/' 'test3_simple/' 'test5_strange/' ;
 do
         pushd "$dir"
+
         if [ -d "build" ]; then rm -r build; fi
-        mkdir build
-        cd build
+        mkdir build &&cd build
+
         cmake -GNinja -DBOARD=native_posix .. 2>&1 1> test.log
+        if [[ $? -ne 0 ]]; then
+            cat test.log
+            exit 1
+        fi
+
         ninja > test.log
+        if [[ $? -ne 0 ]]; then
+            cat test.log
+            exit 1
+        fi
+
         ninja run
+        [[ $? -ne 0 ]] && exit 1
+
         popd
 done
--- tests/cbor_encode/test.sh
+++ tests/cbor_encode/test.sh
@@ -2,12 +2,25 @@
 
 do_test() {
         pushd "$1"
+
         if [ -d "build" ]; then rm -r build; fi
-        mkdir build
-        cd build
+        mkdir build && cd build
+
         cmake -GNinja -DBOARD=native_posix $2 .. 2>&1 1> test.log
+        if [[ $? -ne 0 ]]; then
+            cat test.log
+            exit 1
+        fi
+
         ninja > test.log
+        if [[ $? -ne 0 ]]; then
+            cat test.log
+            exit 1
+        fi
+
         ninja run
+        [[ $? -ne 0 ]] && exit 1
+
         popd
 }
 
