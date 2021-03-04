ee51b17c2158fc9b8507df59b3e62a2a842355e1
Øyvind Rønningstad <oyvind.ronningstad@nordicsemi.no>
2021-03-04 01:14:25 +0100

    tests: scripts: Add test code and modify vectors and cddl
    Some of the vectors needed changes to decode correctly.
    Some was changing the order because of maps, some was fixing errors.
    
    Change the CDDL to be more parsable.
    Changing the order of maps.
    Add "missing" element
    Add CDDL for COSE_Sign1
    
    Add new tests to tests/test.sh and to the Github workflow
    
    Signed-off-by: Øyvind Rønningstad <oyvind.ronningstad@nordicsemi.no>

--- tests/test.sh
+++ tests/test.sh
@@ -7,6 +7,13 @@
 
 pycodestyle ../scripts/cddl_gen.py --max-line-length=120 --ignore=W191,E101,W503
 [[ $? -ne 0 ]] && exit 1
+pycodestyle scripts/run_tests.py --max-line-length=120 --ignore=W503,E501,E402
+[[ $? -ne 0 ]] && exit 1
+
+pushd "scripts"
+python3 -m unittest run_tests
+[[ $? -ne 0 ]] && popd && exit 1
+popd
 
 for dir in 'cbor_decode/' 'cbor_encode/' ;
 do
