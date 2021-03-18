6d7c31402c09dc9a2ed97faf175dc5189f065c87
Øyvind Rønningstad <oyvind.ronningstad@nordicsemi.no>
2021-03-18 15:49:48 +0100

    Add pycodestyle to test runs.
    Signed-off-by: Øyvind Rønningstad <oyvind.ronningstad@nordicsemi.no>

--- tests/test.sh
+++ tests/test.sh
@@ -5,6 +5,9 @@
 # SPDX-License-Identifier: Apache-2.0
 #
 
+pycodestyle ../scripts/cddl_gen.py --max-line-length=120 --ignore=W191,E101,W503
+[[ $? -ne 0 ]] && exit 1
+
 for dir in 'cbor_decode/' 'cbor_encode/' ;
 do
         pushd "$dir"
