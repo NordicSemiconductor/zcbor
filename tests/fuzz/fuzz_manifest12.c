b22d8ff9ad0274ec5f528551ef5dfcb8676ca36c
Øyvind Rønningstad <oyvind.ronningstad@nordicsemi.no>
2021-05-10 20:51:27 +0200

    tests: fuzz: Add fuzz testing of manifest12
    Signed-off-by: Øyvind Rønningstad <oyvind.ronningstad@nordicsemi.no>

---
+++ tests/fuzz/fuzz_manifest12.c
@@ -0,0 +1,136 @@
+#include "manifest12_decode.h"
+#include "main_entry.h"
+
+bool fuzz_one_input(const uint8_t *data, size_t size)
+{
+    uint32_t payload_len_out = 0;
+    struct SUIT_Envelope result;
+    struct SUIT_Envelope result2;
+    struct SUIT_Command_Sequence command_seq;
+
+    bool ret = cbor_decode_SUIT_Envelope_Tagged(data, size,
+                               &result,
+                               &payload_len_out);
+    if (!ret) {
+        return ret;
+    }
+
+    for (int i = 0; i < result._SUIT_Envelope_suit_integrated_dependency_key_count; i++) {
+        ret = cbor_decode_SUIT_Envelope(
+            result._SUIT_Envelope_suit_integrated_dependency_key[i]._SUIT_Envelope_suit_integrated_dependency_key.value,
+            result._SUIT_Envelope_suit_integrated_dependency_key[i]._SUIT_Envelope_suit_integrated_dependency_key.len,
+            &result2, &payload_len_out);
+        if (!ret) {
+            return ret;
+        }
+    }
+
+    if (result._SUIT_Envelope_suit_manifest_cbor
+              ._SUIT_Manifest__SUIT_Severable_Manifest_Members
+              ._SUIT_Severable_Manifest_Members_suit_dependency_resolution_present) {
+        ret = cbor_decode_SUIT_Command_Sequence(
+            result._SUIT_Envelope_suit_manifest_cbor
+                ._SUIT_Manifest__SUIT_Severable_Manifest_Members
+                ._SUIT_Severable_Manifest_Members_suit_dependency_resolution
+                ._SUIT_Severable_Manifest_Members_suit_dependency_resolution.value,
+            result._SUIT_Envelope_suit_manifest_cbor
+                ._SUIT_Manifest__SUIT_Severable_Manifest_Members
+                ._SUIT_Severable_Manifest_Members_suit_dependency_resolution
+                ._SUIT_Severable_Manifest_Members_suit_dependency_resolution.len,
+            &command_seq, &payload_len_out);
+        if (!ret) {
+            return ret;
+        }
+    }
+
+    if (result._SUIT_Envelope_suit_manifest_cbor
+              ._SUIT_Manifest__SUIT_Severable_Manifest_Members
+              ._SUIT_Severable_Manifest_Members_suit_payload_fetch_present) {
+        ret = cbor_decode_SUIT_Command_Sequence(
+            result._SUIT_Envelope_suit_manifest_cbor
+                ._SUIT_Manifest__SUIT_Severable_Manifest_Members
+                ._SUIT_Severable_Manifest_Members_suit_payload_fetch
+                ._SUIT_Severable_Manifest_Members_suit_payload_fetch.value,
+            result._SUIT_Envelope_suit_manifest_cbor
+                ._SUIT_Manifest__SUIT_Severable_Manifest_Members
+                ._SUIT_Severable_Manifest_Members_suit_payload_fetch
+                ._SUIT_Severable_Manifest_Members_suit_payload_fetch.len,
+            &command_seq, &payload_len_out);
+        if (!ret) {
+            return ret;
+        }
+    }
+
+    if (result._SUIT_Envelope_suit_manifest_cbor
+              ._SUIT_Manifest__SUIT_Severable_Manifest_Members
+              ._SUIT_Severable_Manifest_Members_suit_install_present) {
+        ret = cbor_decode_SUIT_Command_Sequence(
+            result._SUIT_Envelope_suit_manifest_cbor
+                ._SUIT_Manifest__SUIT_Severable_Manifest_Members
+                ._SUIT_Severable_Manifest_Members_suit_install
+                ._SUIT_Severable_Manifest_Members_suit_install.value,
+            result._SUIT_Envelope_suit_manifest_cbor
+                ._SUIT_Manifest__SUIT_Severable_Manifest_Members
+                ._SUIT_Severable_Manifest_Members_suit_install
+                ._SUIT_Severable_Manifest_Members_suit_install.len,
+            &command_seq, &payload_len_out);
+        if (!ret) {
+            return ret;
+        }
+    }
+
+    if (result._SUIT_Envelope_suit_manifest_cbor
+              ._SUIT_Manifest__SUIT_Unseverable_Members
+              ._SUIT_Unseverable_Members_suit_validate_present) {
+        ret = cbor_decode_SUIT_Command_Sequence(
+            result._SUIT_Envelope_suit_manifest_cbor
+                ._SUIT_Manifest__SUIT_Unseverable_Members
+                ._SUIT_Unseverable_Members_suit_validate
+                ._SUIT_Unseverable_Members_suit_validate.value,
+            result._SUIT_Envelope_suit_manifest_cbor
+                ._SUIT_Manifest__SUIT_Unseverable_Members
+                ._SUIT_Unseverable_Members_suit_validate
+                ._SUIT_Unseverable_Members_suit_validate.len,
+            &command_seq, &payload_len_out);
+        if (!ret) {
+            return ret;
+        }
+    }
+
+    if (result._SUIT_Envelope_suit_manifest_cbor
+              ._SUIT_Manifest__SUIT_Unseverable_Members
+              ._SUIT_Unseverable_Members_suit_load_present) {
+        ret = cbor_decode_SUIT_Command_Sequence(
+            result._SUIT_Envelope_suit_manifest_cbor
+                ._SUIT_Manifest__SUIT_Unseverable_Members
+                ._SUIT_Unseverable_Members_suit_load
+                ._SUIT_Unseverable_Members_suit_load.value,
+            result._SUIT_Envelope_suit_manifest_cbor
+                ._SUIT_Manifest__SUIT_Unseverable_Members
+                ._SUIT_Unseverable_Members_suit_load
+                ._SUIT_Unseverable_Members_suit_load.len,
+            &command_seq, &payload_len_out);
+        if (!ret) {
+            return ret;
+        }
+    }
+
+    if (result._SUIT_Envelope_suit_manifest_cbor
+              ._SUIT_Manifest__SUIT_Unseverable_Members
+              ._SUIT_Unseverable_Members_suit_run_present) {
+        ret = cbor_decode_SUIT_Command_Sequence(
+            result._SUIT_Envelope_suit_manifest_cbor
+                ._SUIT_Manifest__SUIT_Unseverable_Members
+                ._SUIT_Unseverable_Members_suit_run
+                ._SUIT_Unseverable_Members_suit_run.value,
+            result._SUIT_Envelope_suit_manifest_cbor
+                ._SUIT_Manifest__SUIT_Unseverable_Members
+                ._SUIT_Unseverable_Members_suit_run
+                ._SUIT_Unseverable_Members_suit_run.len,
+            &command_seq, &payload_len_out);
+        if (!ret) {
+            return ret;
+        }
+    }
+    return ret;
+}
--- tests/fuzz/test-afl.sh
+++ tests/fuzz/test-afl.sh
@@ -14,9 +14,10 @@ echo "Usage: $0 <seconds to run> <bit width>"
 if [ -d "build-afl" ]; then rm -r build-afl; fi
 mkdir build-afl
 pushd build-afl
-cmake .. -DCMAKE_C_COMPILER=afl-gcc -DCMAKE_C_FLAGS="-m$2"
+# cmake .. -DCMAKE_C_COMPILER=afl-gcc -DCMAKE_C_FLAGS="-m$2" -DTEST_CASE=pet
+cmake .. -DCMAKE_C_COMPILER=afl-gcc -DCMAKE_C_FLAGS="-m$2" -DTEST_CASE=manifest12
 [[ $? -ne 0 ]] && exit 1
-make fuzz_pet
+make fuzz_target
 [[ $? -ne 0 ]] && exit 1
 popd
-afl-fuzz -i build-afl/fuzz_input -o build-afl/output -V $1 -- ./build-afl/fuzz_pet
+afl-fuzz -i build-afl/fuzz_input -o build-afl/output -V $1 -- ./build-afl/fuzz_target
