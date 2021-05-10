72d41ab75c25c4948d80095a37f580b49d4891d8
Øyvind Rønningstad <oyvind.ronningstad@nordicsemi.no>
2021-05-10 20:51:27 +0200

    tests: fuzz: Add support for fuzz testing with manifest12.cddl
    draft-ietf-suit-manifest-12
    
    Signed-off-by: Øyvind Rønningstad <oyvind.ronningstad@nordicsemi.no>

--- tests/fuzz/test-libfuzzer.sh
+++ tests/fuzz/test-libfuzzer.sh
@@ -7,9 +7,10 @@
 if [ -d "build-libfuzzer" ]; then rm -r build-libfuzzer; fi
 mkdir build-libfuzzer
 pushd build-libfuzzer
-cmake .. -DCMAKE_C_COMPILER=clang -DCMAKE_C_FLAGS="-fsanitize=fuzzer,address -DLIBFUZZER" -DCMAKE_C_COMPILER_WORKS=On
+cmake .. -DCMAKE_C_COMPILER=clang -DCMAKE_C_FLAGS="-fsanitize=fuzzer,address -DLIBFUZZER" -DCMAKE_C_COMPILER_WORKS=On -DTEST_CASE=manifest12
 [[ $? -ne 0 ]] && exit 1
-make fuzz_pet
+make fuzz_target
 [[ $? -ne 0 ]] && exit 1
 popd
-./build-libfuzzer/fuzz_pet -max_total_time=$1
+./build-libfuzzer/fuzz_target -max_total_time=$1
+[[ $? -ne 0 ]] && exit 1
