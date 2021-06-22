91bc226add732845b3c8101d9113ffff949c4188
Øyvind Rønningstad <oyvind.ronningstad@nordicsemi.no>
2021-06-22 15:51:34 +0200

    tests: fuzz: Add support for 32 bit mode in afl
    Signed-off-by: Øyvind Rønningstad <oyvind.ronningstad@nordicsemi.no>

--- tests/fuzz/test-afl.sh
+++ tests/fuzz/test-afl.sh
@@ -4,10 +4,17 @@
 # SPDX-License-Identifier: Apache-2.0
 #
 
+echo "Usage: $0 <seconds to run> <bit width>"
+
+# For convenience (use for extra checks):
+# export AFL_USE_ASAN=1
+# For convenience (use if afl fails and recommends this):
+# export AFL_I_DONT_CARE_ABOUT_MISSING_CRASHES=1
+
 if [ -d "build-afl" ]; then rm -r build-afl; fi
 mkdir build-afl
 pushd build-afl
-cmake .. -DCMAKE_C_COMPILER=afl-clang-fast
+cmake .. -DCMAKE_C_COMPILER=afl-gcc -DCMAKE_C_FLAGS="-m$2"
 [[ $? -ne 0 ]] && exit 1
 make fuzz_pet
 [[ $? -ne 0 ]] && exit 1
