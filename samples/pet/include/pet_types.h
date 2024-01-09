b85a5508d473ef6d301d92eae2d721369a5a7665
Øyvind Rønningstad <oyvind.ronningstad@nordicsemi.no>
2024-01-09 15:19:21 +0100

    Update version from 0.8.1 to 0.8.99
    Signed-off-by: Øyvind Rønningstad <oyvind.ronningstad@nordicsemi.no>

--- samples/pet/include/pet_types.h
+++ samples/pet/include/pet_types.h
@@ -3,7 +3,7 @@
  *
  * SPDX-License-Identifier: Apache-2.0
  *
- * Generated using zcbor version 0.8.1
+ * Generated using zcbor version 0.8.99
  * https://github.com/NordicSemiconductor/zcbor
  * Generated with a --default-max-qty of 3
  */
