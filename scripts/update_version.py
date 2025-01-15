46e4acb2afce810fd0f8fbe9b623fabdc6cd4cd1
Øyvind Rønningstad <oyvind.ronningstad@nordicsemi.no>
2025-01-15 13:11:34 +0100

    Reformat all python files with black
    Modify the TestCodestyle test to use black instead of pycodestyle, to
    avoid disagreements in formatting between the two.
    
    Add black.sh for quickly formatting the codebase.
    
    Signed-off-by: Øyvind Rønningstad <oyvind.ronningstad@nordicsemi.no>

--- scripts/update_version.py
+++ scripts/update_version.py
@@ -10,10 +10,10 @@ from re import sub, match, S
 from datetime import datetime
 
 p_root = Path(__file__).absolute().parents[1]
-p_VERSION = Path(p_root, 'zcbor', 'VERSION')
-p_RELEASE_NOTES = Path(p_root, 'RELEASE_NOTES.md')
-p_MIGRATION_GUIDE = Path(p_root, 'MIGRATION_GUIDE.md')
-p_common_h = Path(p_root, 'include', 'zcbor_common.h')
+p_VERSION = Path(p_root, "zcbor", "VERSION")
+p_RELEASE_NOTES = Path(p_root, "RELEASE_NOTES.md")
+p_MIGRATION_GUIDE = Path(p_root, "MIGRATION_GUIDE.md")
+p_common_h = Path(p_root, "include", "zcbor_common.h")
 
 RELEASE_NOTES_boilerplate = """
 Any new bugs, requests, or missing features should be reported as [Github issues](https://github.com/NordicSemiconductor/zcbor/issues).
@@ -31,23 +31,29 @@ def update_relnotes(p_relnotes, version, boilerplate="", include_date=True):
         new_date = f" ({datetime.today().strftime('%Y-%m-%d')})" if include_date else ""
         relnotes_new_header = f"# zcbor v. {version}{new_date}\n"
         if ".99" not in relnotes_lines[0]:
-            relnotes_contents = relnotes_new_header + boilerplate + '\n\n' + relnotes_contents
+            relnotes_contents = relnotes_new_header + boilerplate + "\n\n" + relnotes_contents
         relnotes_contents = sub(r".*?\n", relnotes_new_header, relnotes_contents, count=1)
         p_relnotes.write_text(relnotes_contents, encoding="utf-8")
 
 
 if __name__ == "__main__":
-    if len(argv) != 2 or match(r'\d+\.\d+\.\d+', argv[1]) is None:
+    if len(argv) != 2 or match(r"\d+\.\d+\.\d+", argv[1]) is None:
         print(f"Usage: {argv[0]} <new zcbor version>")
         exit(1)
     version = argv[1]
-    (major, minor, bugfix) = version.split('.')
+    (major, minor, bugfix) = version.split(".")
 
     p_VERSION.write_text(version, encoding="utf-8")
     update_relnotes(p_RELEASE_NOTES, version, boilerplate=RELEASE_NOTES_boilerplate)
     update_relnotes(p_MIGRATION_GUIDE, version, include_date=False)
     p_common_h_contents = p_common_h.read_text(encoding="utf-8")
-    common_h_new_contents = sub(r"(#define ZCBOR_VERSION_MAJOR )\d+", f"\\g<1>{major}", p_common_h_contents)
-    common_h_new_contents = sub(r"(#define ZCBOR_VERSION_MINOR )\d+", f"\\g<1>{minor}", common_h_new_contents)
-    common_h_new_contents = sub(r"(#define ZCBOR_VERSION_BUGFIX )\d+", f"\\g<1>{bugfix}", common_h_new_contents)
+    common_h_new_contents = sub(
+        r"(#define ZCBOR_VERSION_MAJOR )\d+", f"\\g<1>{major}", p_common_h_contents
+    )
+    common_h_new_contents = sub(
+        r"(#define ZCBOR_VERSION_MINOR )\d+", f"\\g<1>{minor}", common_h_new_contents
+    )
+    common_h_new_contents = sub(
+        r"(#define ZCBOR_VERSION_BUGFIX )\d+", f"\\g<1>{bugfix}", common_h_new_contents
+    )
     p_common_h.write_text(common_h_new_contents, encoding="utf-8")
--- tests/decode/test5_corner_cases/floats.py
+++ tests/decode/test5_corner_cases/floats.py
@@ -12,28 +12,35 @@ import numpy
 import math
 import cbor2
 
-def print_var(val1, val2, bytestr):
-	var_str = ""
-	for b in bytestr:
-		var_str += hex(b)  + ", "
-	print(str(val1) + ":", val2, bytestr.hex(), var_str)
 
-def print_32_64(str_val, val = None):
-	val = val or float(str_val)
-	print_var(str_val, val, struct.pack("!e", numpy.float16(val)))
-	print_var(str_val, val, struct.pack("!f", struct.unpack("!e", struct.pack("!e", numpy.float16(val)))[0]))
-	print (numpy.float32(struct.unpack("!e", struct.pack("!e", numpy.float16(val)))[0]))
-	print_var(str_val, val, struct.pack("!f", val))
-	print_var(str_val, val, struct.pack("!d", val))
-	print_var(str_val, val, struct.pack("!d", struct.unpack("!f", struct.pack("!f", val))[0]))
-	print()
+def print_var(val1, val2, bytestr):
+    var_str = ""
+    for b in bytestr:
+        var_str += hex(b) + ", "
+    print(str(val1) + ":", val2, bytestr.hex(), var_str)
+
+
+def print_32_64(str_val, val=None):
+    val = val or float(str_val)
+    print_var(str_val, val, struct.pack("!e", numpy.float16(val)))
+    print_var(
+        str_val,
+        val,
+        struct.pack("!f", struct.unpack("!e", struct.pack("!e", numpy.float16(val)))[0]),
+    )
+    print(numpy.float32(struct.unpack("!e", struct.pack("!e", numpy.float16(val)))[0]))
+    print_var(str_val, val, struct.pack("!f", val))
+    print_var(str_val, val, struct.pack("!d", val))
+    print_var(str_val, val, struct.pack("!d", struct.unpack("!f", struct.pack("!f", val))[0]))
+    print()
+
 
 print_32_64("3.1415")
 print_32_64("2.71828")
 print_32_64("1234567.89")
 print_32_64("-98765.4321")
-print_32_64("123/456789", 123/456789)
-print_32_64("-2^(-42)", -1/(2**(42)))
+print_32_64("123/456789", 123 / 456789)
+print_32_64("-2^(-42)", -1 / (2 ** (42)))
 print_32_64("1.0")
 print_32_64("-10000.0")
 print_32_64("0.0")
